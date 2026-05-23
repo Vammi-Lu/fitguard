c4a7741abe739d7a84339bed0e334da9c5c45ce0
Vammi-Lu <dommerenserdeg@yandex.ru>
2026-05-23 21:01:14 +0400

    Я сплю
    Co-authored-by: Я́лла Нкарда́з д͏е Туде́рий <DemerNkardaz@users.noreply.github.com>

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
+
+<template>
+	<div>
+		test
+	</div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,37 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import scssTokensPlugin from 'vite-plugin-css-vars-to-scss-tokens';
+
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '')
-	
+
 	return {
 		plugins: [
 			vue(),
-			vueDevTools()
+			vueDevTools(),
+			scssTokensPlugin()			
 		],
+
 		base: env.VITE_BASE_URL,
+
 		resolve: {
 			alias: {
 				"@": fileURLToPath(new URL("./src", import.meta.url)),
 			},
 		},
-	};
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+					@use "@/styles/tokens" as *;
+					@use "@/styles/constants" as *;
+					@use "@/styles/functions" as *;
+					@use "@/styles/mixins" as *;
+				`,
+				},
+			},
+		},
+	}
 })
